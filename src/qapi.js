b5d311005322addce8f4f5a6ecd5a48511647ec3
Rohit Jadhav <45796556+rohit746@users.noreply.github.com>
2021-02-21 11:44:23 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fcb0f1de563f3001ecb6a7d",
+				"5fcb0f1de563f3001ecb6a7f"
+			]
+		},
+		"5fcb0f1de563f3001ecb6a7d": {
+			"id": "5fcb0f1de563f3001ecb6a7d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fcb0f1de563f3001ecb6a7f": {
+			"id": "5fcb0f1de563f3001ecb6a7f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
